188559593d78d1b3ad3cf76f91e01e09c4f3bde5
Green Alexandr <orelne7@gmail.com>
2021-03-15 20:34:40 +0300

    add visit,users and contacts

--- app.rb
+++ app.rb
@@ -13,3 +13,22 @@ end
 get '/visit' do
 	erb :visit
 end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/visit' do
+        @user_name = params[:user_name] 
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+
+input = File.open("./public/users.txt", 'a')
+input.write "User_name: #{@user_name}, Phone: #{@phone}, Date & time: #{@date_time}\n"
+input.close
+
+	@title = 'Thank you!'
+	@message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}"
+
+	erb :message
+end
