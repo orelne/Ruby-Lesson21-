4f11dfb273beaf4e96d6639edc7f7ae2abc5f890
Green Alexandr <orelne7@gmail.com>
2021-03-19 23:09:22 +0300

    add login form

--- app.rb
+++ app.rb
@@ -31,13 +31,20 @@ post '/visit' do
 				 :date_time => 'Введите дату и время' }
 
 
-		hh.each do |key, value|
-			if params[key] == ''
-				@error = hh[key]
-				return erb :visit
+		@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+		if @error != ''
+			return erb :visit
+		end
+
+
+	#	hh.each do |key, value|
+	#		if params[key] == ''
+	#			@error = hh[key]
+	#			return erb :visit
 					 	
-			 end
-		end		 
+	#		 end
+	#	end			 
 
 			
 input = File.open("./public/users.txt", 'a')
@@ -52,4 +59,46 @@ input.close
 	@message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}"
 
 	erb :message
+end
+
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  unless session[:identity]
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+end
+
+get '/' do
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+end
+
+get '/login/form' do
+  erb :login_form
+end
+
+post '/login/attempt' do
+  session[:identity] = params['username']
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from
+end
+
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+get '/secure/place' do
+  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
 end
